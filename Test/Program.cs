b76fcbb7f0dda6f8021c4aec5608745a9d98a515
Ilie Laurențiu-Lucian <ilielaurentiu8@yahoo.com>
2024-04-15 17:54:16 +0300

    adaugat comentarii

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,11 +4,13 @@
     {
         static void Main(string[] args)
         {
-            // Am creat si initializat un vector de stack-uri (coloane) pentru a contine cutiile
+
+            // Am creat si initializat un vector de stack-uri (coloane) de tip generic string, pentru a contine cutiile
             Stack<string>[] Coloana = [new Stack<string>(), new Stack<string>(), new Stack<string>()];
+            // definim un sir de caractere pe care il folosim la citirea rand cu rand din fisier
             string linieFisier;
 
-            // Introducem valorile din problema in ordine inversa, in fiecare stack
+            // Introducem valorile din problema in ordine inversa, in fiecare coloana
             Coloana[0].Push("Z");
             Coloana[0].Push("N");
 
@@ -18,33 +20,47 @@
 
             Coloana[2].Push("P");
 
-            StreamReader streamReader = new("D:\\Sample.txt");
-            linieFisier = streamReader.ReadLine();
+            // initializam un cititor de fisier
+            StreamReader cititorFisier = new("D:\\Sample.txt");
+            // introducem in linieFisier sirul de caractere de pe prima linie
+            linieFisier = cititorFisier.ReadLine();
 
+            // cat timp fisierul nu s-a terminat (linia citita este diferita de null) continuam sa citim
             while (linieFisier != null)
             {
-                Console.WriteLine(linieFisier);
-                //MutaCutii(linieFisier[5], Coloana[linieFisier[12] - 1], Coloana[linieFisier[17] - 1]);
+                // apelam functia muta cutii 
+                // desi este o cale foarte rigida, voi lua direct fiecare caracter de pe pozitia 5, 12 si 17, adica numarul de cutii si primele doua coloane din instructiune
+                // de pe fiecare linie, pentru a evita sa parcurg sau sa fac alte operatii cu siruri de caractere
 
-                linieFisier = streamReader.ReadLine();
+                
+
+                MutaCutii(linieFisier[5], Coloana[int.Parse(linieFisier[12].ToString()) - 1], Coloana[int.Parse(linieFisier[17].ToString()) - 1]);
+
+                // linieFisier devine linia urmatoare din fisierul citit
+                linieFisier = cititorFisier.ReadLine();
             }
 
-            streamReader.Close();
+            // Inchidem fisierul
+            cititorFisier.Close();
 
-            TrimiteMesajElfilor($"{Coloana[0].Pop()}{Coloana[1].Pop()}{Coloana[2].Pop()}");
+            // La final afisam mesajul catre elfi folosind functia Peek() la fiecare coloana, pentru a returna doar valoarea cutiei de deasupra, fara modificarea coloanei
+            Console.WriteLine($"{Coloana[0].Peek()}{Coloana[1].Peek()}{Coloana[2].Peek()}");
         }
 
-        public static void MutaCutii(int numarCutii, Stack<string> dinStack, Stack<string> peStack)
+        // cream o functie statica pentru a putea sa o accesam usor
+        // care ia ca argumente numarul de cutii si 2 coloane, din prima va lua cutii si le va pune in a doua
+        public static void MutaCutii(int numarCutii, Stack<string> iaDinColoana, Stack<string> mutaInColoana)
         {
+            // parcurgem aceasta operatie pentru fiecare cutie, data de numarCutii
             for (int i = 0; i < numarCutii; i++)
             {
-                peStack.Push(dinStack.Pop());
+                // scoatem 'cutia' din stackul iaDinColoana si o punem in mutaInColoana
+                Console.WriteLine($"Moving {numarCutii} boxes from column {iaDinColoana} to column {mutaInColoana}");
+                mutaInColoana.Push(iaDinColoana.Pop());
             }
         }
-
-        public static void TrimiteMesajElfilor(string mesaj)
-        {
-            Console.WriteLine(mesaj);
-        }
     }
+
+    // Complexitatea spatiu-timp a programului este lineara, O(n)
+    // deoarece aceasta creste direct proportional cu input-ul, sau in cazul nostru numarul de cutii
 }
